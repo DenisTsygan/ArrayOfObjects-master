640498b7ce2e5828e2fd26855ed8d81f03c4bfb4
DenisTsygan <denis.www.denis7@gmail.com>
2022-04-19 21:01:03 +0300

    Initial commit

---
+++ src/opu/Users.java
@@ -0,0 +1,88 @@
+package opu;
+
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDate;
+import java.time.chrono.ChronoLocalDateTime;
+
+import static opu.utilityPasswordMaker.generatePassword;
+
+public class Users {
+   private final int DAYS=2;
+   private User[] users;
+   private int currentUserNumber;
+
+
+   public Users(final int usersNumber) {
+      this.users = new User[usersNumber];
+      currentUserNumber = -1;
+   }
+
+   public User findOldestUser() {
+      User user1 = users[0];
+
+      for (final User user : users) {
+         if (user.getRegistrationDate().isBefore(user1.getRegistrationDate())) user1 = user;
+      }
+
+      return user1;
+   }
+
+
+   public void addUser(final User user) {
+      if (++currentUserNumber < users.length) {
+         users[currentUserNumber] = user;
+      }
+      CheckDayRegistration(user);
+
+   }
+
+   private void CheckDayRegistration(User user) {
+      if(user.getRegistrationDate().plusDays(DAYS).isBefore(LocalDateTime.now())){
+          user.setPassword(generatePassword());
+      }
+   }
+
+   private void delUserByNumber(final int number) {
+      final Users tempUsers = new Users(currentUserNumber);
+      tempUsers.currentUserNumber = currentUserNumber;
+
+      int newIndex = 0;
+      for (int i = 0; i <= currentUserNumber; i++) {
+         if (i == number) continue;
+         tempUsers.users[newIndex++] = users[i];
+      }
+      users = tempUsers.users;
+      currentUserNumber--;
+   }
+
+   public void removeUser(final int number) {
+      if (number < 0 || number >= currentUserNumber) {
+         return;
+      } else {
+         delUserByNumber(number);
+      }
+   }
+
+   public void removeUser(final String login) {
+      int number = 0;
+      for (final User user : users) {
+         if (user.getLogin().equalsIgnoreCase(login.strip())) {
+            delUserByNumber(number);
+
+            return;
+         }
+         number++;
+      }
+   }
+
+   @Override
+   public String toString() {
+      String result = "Users: " + System.lineSeparator();
+
+      for (final User user : users) {
+         result += user + System.lineSeparator();
+      }
+
+      return result;
+   }
+}
